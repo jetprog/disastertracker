01a9d7fdce0e1fbed61dce4782bc36f61c4e2aef
Ashleigh Kessel <AshleighKessel@gmail.com>
2018-08-09 17:27:21 -0700

    Set up of client files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 app.get('/home', function(req, res) {
     res.send('Server running');
 })
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
+});
 
 const port = process.env.PORT || 3000;
 
