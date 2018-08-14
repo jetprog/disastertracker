47ba895f036d6410a32c472c2327f991a0089ad6
Josiah Liciaga <35929809+Jliciaga@users.noreply.github.com>
2018-08-14 14:39:41 -0400

    changes to database join tables, added testing

--- database/models/models.js
+++ database/models/models.js
@@ -3,76 +3,76 @@ const db = require('../config.js');
 var friendsList = db.Model.extend({
     tableName:'friendsList',
     hasTimeStamps: true,
+    friends: function() {
+        return this.belongsTo(user, 'user_id');
+    },
 });
 
 var user = db.Model.extend({
     tableName: 'users',
     hasTimeStamps: true,
-    contacts: function() {
-        return this.belongsTo(contactList, 'user_id');
-    },
-    friends: function() {
-        return this.belongsTo(friendsList, 'user_id');
-    },
-    watchList: function() {
-        return this.belongsTo(watchList, 'user_id');
-    },
-    currentLocation: function() {
-        return this.belongsTo(currentLocation, 'user_id');
-    },
 });
 
 var contactList = db.Model.extend({
     tableName:'contactList',
     hasTimeStamps: true,
+    userID: function() {
+        return this.belongsTo(user, 'user_id');
+    },
+    contactID: function() {
+        return this.belongsTo(contactList, 'contact_id');
+    },
 });
 
 var contact = db.Model.extend({
     tableName: 'contact',
     hasTimeStamps: true,
-    contactList: function() {
-        return this.belongsTo(contactList, 'contact_id');
-    },
 });
 
 var currentLocation = db.Model.extend({
     tableName: '',
     hasTimeStamps: true,
+    userID: function() {
+        return this.belongsTo(user, 'user_id');
+    },
+    locationID: function() {
+        return this.belongsTo(locations, 'location_id')
+    },
 });
 
 var locations = db.Model.extend({
     tableName: '',
     hasTimeStamps: true,
-    geolocation: function() {
-        return this.belongsTo(currentLocation, 'location_id')
-    },
 });
 
 var watchList = db.Model.extend({
     tableName:'contactList',
     hasTimeStamps: true,
+    userID: function() {
+        return this.belongsTo(user, 'user_id');
+    },
+    eventID: function() {
+        return this.belongsTo(event, 'event_id');
+    },
 });
 
 var event = db.Model.extend({
     tableName: 'event',
     hasTimeStamps: true,
-    watchList: function() {
-        return this.belongsTo(watchList, 'event_id');
-    },
-    category: function() {
-        return this.belongsTo(eventList, 'event_id');
-    },
 });
 
 var eventList = db.Model.extend({
     tableName: 'category',
     hasTimeStamps: true,
+    eventID: function() {
+        return this.belongsTo(eventID, 'event_id');
+    },
+    categoryID: function() {
+        return this.belongsTo(category, 'category_id');
+    },
 });
 
 var category = db.Model.extend({
     tableName: 'contact',
     hasTimeStamps: true,
-    eventList: function() {
-        return this.belongsTo(eventList, 'category_id');
-    },
 });
