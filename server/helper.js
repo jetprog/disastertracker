5ce84757b12038aeca5a6e32fc4a8a5e045fcace
Seth Jetro <jsethjetro@gmail.com>
2018-08-15 11:36:51 -0500

    fix facebook login

--- server/helper.js
+++ server/helper.js
@@ -2,9 +2,15 @@ const db = require('../../database/utils');
 
 const login = (req, res) =>{
   db.saveFacebookUser(req.body, (err, user)=>{
-    req.session.regenerate(() =>
-      res
-      .status(200)
-      .send((req.session.user = req.body.username) && req.session))
-  })
+    if(err){
+      res.status(400).send('User can\'t login');
+    }else{
+      req.session.regenerate(() =>
+        res
+        .status(200)
+        .send((req.session.user = req.body.username) && req.session)
+      )
+    }
+  }
+
 }
