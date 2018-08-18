fe76a9ac81541b3bd719918774d7c2d62a577257
Andy Riesenbach <andy.riesenbach@gmail.com>
2018-08-18 19:00:42 -0400

    LocationCardAlerts

--- client/src/components/LocationCardAlerts.jsx
+++ client/src/components/LocationCardAlerts.jsx
@@ -1,6 +1,6 @@
-importReact, {Component} from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
-
+import Typography from '@material-ui/core/Typography'
 
 export default class LocationCardAlerts extends Component {
   constructor (props) {
@@ -8,40 +8,45 @@ export default class LocationCardAlerts extends Component {
     this.state = {
       alerts: []
     }
-    this.parseAlerts = this.parseAlerts.bind(this);
-    this.getLocationAlerts = this.getLocationAlerts.bind(this);
+    this.parseAlerts = this.parseAlerts.bind(this)
+    this.getLocationAlerts = this.getLocationAlerts.bind(this)
   }
 
-  componentDidMount(){
-    this.getLocationAlerts()
+  componentDidMount () {
+    this.getLocationAlerts(this.parseAlerts)
     // const locationAlertInterval = setInterval(this.getLocationAlerts,  10 * 60 * 1000)
   }
-  componentWillUnmount(){
-    //clearInterval(locationAlertInterval)
+  componentWillUnmount () {
+    // clearInterval(locationAlertInterval)
 
   }
-  getLocationAlerts () {
-    params = {
+  getLocationAlerts (callback) {
+    let params = {
       active: 1,
       severity: 'severe',
-      point = `${this.props.latitude},${this.props.longitude}`
-     }
+      point: `${this.props.latitude},${this.props.longitude}`
+    }
     axios.get('https://api.weather.gov/alerts', params)
-    .then(result => this.parseAlerts(null, result))
-    .catch(err => this.parseAlerts(null, result))
+      .then(result => callback(null, result))
+      .catch(err => callback(err, null))
   }
 
-  parseAlerts(err, alerts){
-    var alertArray;
+  parseAlerts (err, alerts) {
+    if (err) { }
+    var alertArray
     if (!alerts.features) {
-      return this.state.alerts.length !== 0 && this.setState( {alerts: []} )
+      return this.state.alerts.length !== 0 && this.setState({alerts: []})
     }
-    alertArray = alerts.features.map(alert => alert.properties);
-    alertArray = alertArray.filter(alert => alert.status !== 'Test' && alert.status !== 'Cancel')
+    alertArray = alerts.features.map(alert => alert.properties)
+    // alertArray = alertArray.filter(alert => alert.status !== 'Test' && alert.status !== 'Cancel')
+    console.log('alerts array is ', alertArray)
+    this.setState({alerts: alertArray})
   }
 
   render () {
-    return this.state.alerts.length === 0 ? (<span>No active alerts at this time</span>) :
-    ( {alerts.map(alert => (<div>{alert.event}</div>))})
+    let {alerts} = this.state
+    console.log(alerts)
+    if (alerts.length === 0) { return (<Typography>No active alerts at this time</Typography>) }
+    return alerts.map(alert => (<Typography>{alert.event}</Typography>))
   }
 }
