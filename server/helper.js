150db031d00c4991037d8b472268fa6e1e848f92
Andy Riesenbach <andy.riesenbach@gmail.com>
2018-08-21 19:20:18 -0400

    sessionUser app now uses correct data, session log in from stored userid

--- server/helper.js
+++ server/helper.js
@@ -1,83 +1,68 @@
-const db = require('../database/utils.js');
+const db = require('../database/utils.js')
 
-//Create user login process
-const login = (req, res) =>{
-  if(req.body.type && req.body.type === "social"){
+// Create user login process
+const login = (req, res) => {
+  if (req.body.type && req.body.type === 'social') {
     db.getUserInfo(req.body.email)
-    .then((user) => {
-      if(user){
-        req.session.regenerate(() =>
-          res
-          .status(200)
-          .send(req.session.user = user))
-      }else {
-        db.saveUser(req.body)
-        .then((user) => {
+      .then(user => {
+        if (user) {
           req.session.regenerate(() =>
-            res
-            .status(200)
-            .send(req.session.user = user))
-        })
-        .catch(function (err) {
-          res.status(400).send({ serverMessage: "User alredy exists", error: err })
-        });
+            res.status(200).send((req.session.user = user))
+          )
+        } else {
+          db.saveUser(req.body)
+            .then(user => {
+              req.session.regenerate(() =>
+                res.status(200).send((req.session.user = user))
+              )
+            })
+            .catch(function (err) {
+              res
+                .status(400)
+                .send({ serverMessage: 'User alredy exists', error: err })
+            })
+        }
+      })
+      .catch(function (err) {
+        res.status(400).send({ serverMessage: 'Wrong username', error: err })
+      })
+  } else {
+    db.getUserInfo(req.body.email, userData => {
+      // console.log(userData)
+      if (userData && userData.password === req.body.password) {
+        delete userData.password
+        req.session.regenerate(() => {
+          req.session.user = userData.user_id
+          res.status(200).send((userData))
+        }
+        )
+      } else {
+        res.status(400).send('Wrong password')
       }
+    })
+  }
+}
+
+const logout = (req, res) => req.session.destroy(() => res.sendStatus(200))
+
+// Signup method for user
+const signup = (req, res) => {
+  db.saveUser(req.body)
+    .then(user => {
+      req.session.regenerate(() =>
+        res.status(200).send((req.session.user = user))
+      )
     })
     .catch(function (err) {
-      res.status(400).send({ serverMessage: "Wrong username", error: err })
-    });
-  }
-  else{
-    db.getUserInfo(req.body.email, function(model){
-      console.log(model);
-      if(model && model.password === req.body.password){
-        delete model.password;
-        req.session.regenerate(() =>
-          res
-          .status(200)
-          .send(req.session.user = model))
-      }
-      else{
-        res.status(400).send('Wrong password');
-      }
+      res.status(400).send({ serverMessage: 'User alredy exists', error: err })
     })
-  }
 }
 
-const logout = (req, res) => req.session.destroy(() => res.sendStatus(200));
+// Middleware make sure user is logged
+const checkLoggedIn = (req, res, next) =>
+  req.session.user ? next() : res.status(401).send('user not logged in')
 
-//Signup method for user
-const signup = (req, res) =>{
-  db.saveUser(req.body)
-  .then((user) => {
-    req.session.regenerate(() =>
-      res
-      .status(200)
-      .send(req.session.user = user))
-  })
-  .catch(function (err) {
-    res.status(400).send({ serverMessage: "User alredy exists", error: err })
-  });
-
-}
-
-//Middleware make sure user is logged
-const checkLoggedIn = (req, res, next) => (req.session.user ? next() : res.status(401).send('user not logged in'));
-
-exports.login = login;
-exports.logout = logout;
-exports.signup = signup;
-exports.checkLoggedIn = checkLoggedIn;
-
-
-
-
-
-
-
-
-
-
-
-
-
+exports.login = login
+exports.logout = logout
+exports.signup = signup
+exports.checkLoggedIn = checkLoggedIn
