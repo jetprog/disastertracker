25c365f9b7c7878c33ef5025369b707c471da0e1
Seth Jetro <jsethjetro@gmail.com>
2018-08-20 20:37:52 -0500

    fix user signup

--- server/helper.js
+++ server/helper.js
@@ -56,7 +56,7 @@ const signup = (req, res) =>{
     req.session.regenerate(() =>
       res
       .status(200)
-      .send(req.session.user = req.body))
+      .send(req.session.user = user))
   })
   .catch(function (err) {
     res.status(400).send({ serverMessage: "User alredy exists", error: err })
