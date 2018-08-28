60226091b10b11aaf219a5f59a89ce36b08dc28f
Seth Jetro <jsethjetro@gmail.com>
2018-08-28 10:15:20 -0500

    basic object and method to save event

---
+++ database/event.js
@@ -0,0 +1,48 @@
+const Events = require('./collections/users.js');
+const Event = require('./models/users.js');
+
+// Save event
+exports.saveEvent = (event) => {
+  new Event({})
+    .save(event, { method: 'insert' })
+    .then((saveEvent) => console.log(saveEvent))
+    .catch(error => console.log(error))
+}
+
+//save coordinates
+exports.saveCoordinates = (coordinate) => {
+  new Coordinates({})
+    .save(coordinate, { method: 'insert' })
+    .then((saveCoordinates) => {
+      console.log(saveCoordinates)
+      //just save
+    })
+    .catch(error => console.log(error))
+}
+
+
+//save multipolygon
+exports.saveMultiPolygon = (multiPolygon) => {
+  new MultiPolygon({})
+    .save(multipolygon, { method: 'insert' })
+    .then((saveMultipolygon) => {
+      // console.log(saveMultipolygon);
+      //get id from multipolygon pass to polygon and event
+      //call savePolygone
+    })
+    .catch(error => console.log(error))
+}
+
+//save polygon
+exports.savePolygon = (polygon) => {
+  new Polygon({})
+    .save(polygon, { method: 'insert' })
+    .then((savePolygon) =>{
+      // console.log(savePolygon)
+      //get polygon id
+      //pass id to coordinates table
+      //call saveCoordinates
+    })
+    .catch(error => console.log(error))
+}
+
