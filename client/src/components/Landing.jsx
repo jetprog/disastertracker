8db8af23180b773ec67316164c5ce0def5ef775d
Josiah Liciaga <35929809+Jliciaga@users.noreply.github.com>
2018-08-30 16:06:08 -0400

    changed styling for UI, took out animations

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -10,12 +10,15 @@ class Landing extends Component {
     return(
       <div>
       <section id="hero">
-      <img id="hero-image" src="./assets/storm.jpg" />
+      <img id="hero-image" src="./assets/fire.jpg" />
       <div id="hero-content">
        <h1>Disaster Tracker</h1>
        <p>Know when danger is coming right from your phone</p>
        </div>
        </section>
+       <div class="sticky-footer">
+        Powered by @Team-thesis1
+      </div>
       </div>
     )
   }
