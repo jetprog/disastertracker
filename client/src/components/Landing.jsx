bf8daf63a8982f82b7e1236f1a5c4228c87e4bbd
Josiah Liciaga <35929809+Jliciaga@users.noreply.github.com>
2018-08-30 13:49:07 -0400

    changes to UI

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -15,25 +15,7 @@ class Landing extends Component {
        <h1>Disaster Tracker</h1>
        <p>Know when danger is coming right from your phone</p>
        </div>
-      </section>
-      <div id="about">
-        <h4>About</h4>
-        <img id="example" src='./assets/example.png' />
-        <p>There are applications out there that will allow you to look up weather but none that will notify you for the many different locations this world has to offer. With Disaster tracker, add locations for friends and family members when there is any major inclement weather happening anywhere near them, even when the user is not logged in. Rest easier with Disaster tracker, it has your back! </p>
-      </div>
-      <div id="feature-list">
-      <h4>Feature List</h4>
-      <ul>
-        <li>Geolocation</li>
-        <li>Location based alerts</li>
-        <li>Alerts for friends and family</li>
-      </ul>
-      </div>
-      <div id="footer">
-      <div class="sticky-footer">
-        Powered by @team-disaster
-      </div>
-      </div>
+       </section>
       </div>
     )
   }
